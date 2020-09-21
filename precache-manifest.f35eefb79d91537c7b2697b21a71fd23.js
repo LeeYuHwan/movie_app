eccd1816c19347b675b92299fd81d9dc7727cf73
LeeYuHwan <dbghksskfk@naver.com>
2020-09-22 00:40:10 +0900

    Updates

---
+++ precache-manifest.f35eefb79d91537c7b2697b21a71fd23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "133a2c2641902783e60c9f616e6c9547",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "c4221f4153d76862f387",
+    "url": "/movie_app/static/css/main.c471333c.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4221f4153d76862f387",
+    "url": "/movie_app/static/js/main.f3125d9c.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
